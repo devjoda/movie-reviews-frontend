913c1ebe2f53da653f33811370cff59274fb5bea
Joachim <45370332+devjoda@users.noreply.github.com>
2022-02-24 15:03:09 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+
+// page & layout imports
+import Homepage from './pages/Homepage'
+import ReviewDetails from './pages/ReviewDetails'
+import Category from './pages/Category'
+import SiteHeader from './components/SiteHeader'
+
+// apollo client
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache(),
+})
+
+function App() {
+  return (
+    <div className='App'>
+      <ApolloProvider client={client}>
+        <SiteHeader />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/api/details/:id' element={<ReviewDetails />} />
+          <Route path='/api/categories/:id' element={<Category />} />
+        </Routes>
+      </ApolloProvider>
+    </div>
+  )
+}
+
+export default App
